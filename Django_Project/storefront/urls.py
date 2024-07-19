9273c3391ba1c9e7db877a1a3749c7f57aba7d52
Sam Chan <kaisum.chan1@gmail.com>
2024-07-19 23:49:33 +0800

    First Django Project Developed

--- Django_Project/storefront/urls.py
+++ Django_Project/storefront/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from . import views
+import debug_toolbar
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('playground/', include('playground.urls')),
+    path('', views.home, name='home'),
+    path('__debug__/', include(debug_toolbar.urls))
 ]
